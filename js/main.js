9b075fb4f6eeb93be8654cf864ba3bff9cbdf754
Ken Tauchi <kentauchica@gmail.com>
2021-11-16 22:49:15 -0800

    activate player

--- js/main.js
+++ js/main.js
@@ -5,18 +5,23 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+//=============== Global Variables =================
 const start_position = 3;
 const end_position = -start_position;
+const text = document.querySelector(".text");
+const TIME_LIMIT = 20;
+let gameStat = "loading";
+let isLookingBackward = false;
 
-const createCube = (size, rot, positionX, color = 0xfbc851) => {
+// ==================================================
+
+const createCube = (size, positionX, rotY = 0, color = 0xfbc851) => {
   const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
-  const material = new THREE.MeshBasicMaterial({ color: color });
+  const material = new THREE.MeshBasicMaterial({ color });
   const cube = new THREE.Mesh(geometry, material);
   cube.position.x = positionX;
-  cube.rotation.x = rot.x;
-  cube.rotation.y = rot.y;
+  cube.rotation.y = rotY;
   scene.add(cube);
-
   return cube;
 };
 
@@ -26,6 +31,10 @@ renderer.setClearColor(0xb7c3f3, 1);
 const loader = new THREE.GLTFLoader();
 const light = new THREE.AmbientLight(0xffffff); // soft white light
 scene.add(light);
+
+const delay = (ms) => {
+  return new Promise((res) => setTimeout(res, ms));
+};
 
 class Doll {
   constructor() {
@@ -38,12 +47,19 @@ class Doll {
   }
 
   lookBackward() {
-    // this.doll.rotation.y = -3.15;
     gsap.to(this.doll.rotation, { y: -3.15, duration: 0.45 });
+    setTimeout(() => (isLookingBackward = true), 450);
   }
   lookForward() {
-    // this.doll.rotation.y = 0;
     gsap.to(this.doll.rotation, { y: 0, duration: 0.45 });
+    setTimeout(() => (isLookingBackward = false), 150);
+  }
+  async start() {
+    this.lookBackward();
+    await delay(Math.random() * 1000 + 1000);
+    this.lookForward();
+    await delay(Math.random() * 1000 + 1000);
+    this.start();
   }
 }
 
@@ -55,30 +71,82 @@ class Player {
     sphere.position.z = 1.4;
     sphere.position.x = start_position;
     scene.add(sphere);
+    this.player = sphere;
+    this.playerInfo = {
+      positionX: start_position,
+      velocity: 0,
+    };
+  }
+  run() {
+    this.playerInfo.velocity = 0.03;
+  }
+  stop() {
+    gsap.to(this.playerInfo, { velocity: 0, duration: 0.5 });
+  }
+  check() {
+    if (this.playerInfo.velocity > 0 && isLookingBackward) {
+      gameStat = "over";
+      text.innerText = "Shots fired!";
+    }
+    if (this.playerInfo.positionX < end_position) {
+      gameStat = "over";
+      text.innerText = "Survived";
+    }
+  }
+  update() {
+    this.check();
+    this.playerInfo.positionX -= this.playerInfo.velocity;
+    this.player.position.x = this.playerInfo.positionX;
   }
 }
 
 const player = new Player();
 
 const createTrack = () => {
-  createCube({ w: start_position * 2, h: 1.5, d: 1 }, { x: 0, y: 0 }, 0, 0xe5a716).position.z =
-    -0.8;
-  createCube({ w: 0.2, h: 1.5, d: 1 }, { x: 0, y: 0 }, start_position);
-  createCube({ w: 0.2, h: 1.5, d: 1 }, { x: 0, y: 0 }, end_position);
+  createCube({ w: start_position * 2 + 0.21, h: 1.5, d: 1 }, 0, 0, 0xe5a716).position.z = -1;
+  createCube({ w: 0.2, h: 1.5, d: 1 }, start_position, -0.4);
+  createCube({ w: 0.2, h: 1.5, d: 1 }, end_position, 0.4);
 };
 
 createTrack();
 
 let doll = new Doll();
-setTimeout(() => {
-  doll.lookBackward();
-}, 1000);
 
-function animate() {
-  requestAnimationFrame(animate);
+const startGame = () => {
+  let progressBar = createCube({ w: 5, h: 0.1, d: 1 }, 0);
+  progressBar.position.y = 3.35;
+  gsap.to(progressBar.scale, { x: 0, duration: TIME_LIMIT });
+  doll.start();
+  setTimeout(() => {
+    if (gameStat !== "over") {
+      text.innerText = "You ran out of time, shots fired";
+      gameStat = "over";
+    }
+  }, TIME_LIMIT * 1000);
+  gameStat = "started";
+};
 
+const init = async () => {
+  await delay(1000);
+  text.innerText = "Starting in 3";
+  await delay(1000);
+  text.innerText = "Starting in 2";
+  await delay(1000);
+  text.innerText = "Starting in 1";
+  await delay(1000);
+  text.innerText = "Gooo";
+  await delay(1000);
+  startGame();
+};
+
+const animate = () => {
+  if (gameStat == "over") return;
   renderer.render(scene, camera);
-}
+  requestAnimationFrame(animate);
+  player.update();
+};
+
+init();
 animate();
 
 const onWindowResize = () => {
@@ -88,3 +156,16 @@ const onWindowResize = () => {
 };
 
 window.addEventListener("resize", onWindowResize, false);
+
+window.addEventListener("keydown", (e) => {
+  if (gameStat !== "started") return;
+  if (e.key === "ArrowUp") {
+    player.run();
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.key === "ArrowUp") {
+    player.stop();
+  }
+});
